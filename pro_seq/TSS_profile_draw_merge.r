459657522183133cd931bc6f43eda450305ebc4d
lbyybl <34057635+lbyybl@users.noreply.github.com>
2022-09-13 15:52:30 +0800

    Update TSS_profile_draw_merge.r

--- pro_seq/TSS_profile_draw_merge.r
+++ pro_seq/TSS_profile_draw_merge.r
@@ -1,7 +1,6 @@
 #---- Pro-seq profile 
 rm(list=ls())
-# setwd('/WORK/lbyybl/WH/rvb/pro-seq/Ruvbl2/sample20190921/graph/TSS_profile/')
-setwd('/WORK/lbyybl/WH/rvb/pro-seq/Ruvbl2/sample20200113/graph/TSS_profile')
+setwd('') # set wrok dir
 trans_df <- function(data,name){
   data <- abs(data)
   colnames(data) <- paste0('c',0:5999)
@@ -45,9 +44,6 @@ doxr_mean<- trans_df(dox2,'dox')
 h05r_mean <- trans_df(h052,'h05')
 h1r_mean <- trans_df(h12,'h1')
 
-plot(-dox_mean$coor,-dox_mean$value,type='l')
-# plot(dox$coor,dox$value,type='l',xlim=c(-3000,3000),ylim=c(0,100))
-
 merge <- rbind(dox_mean,h05_mean)
 merge <- rbind(merge,h1_mean)
 
@@ -65,85 +61,5 @@ ggplot(merge,aes(coor,value,color=sample))+geom_line()+
   scale_x_continuous(limits = xrange)+
   theme(plot.title=element_text(hjust=0.5),
         panel.grid=element_blank(), panel.background=element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=0.2)) #+
-  # geom_vline(xintercept =c(90,330),color='blue')
+        panel.border = element_rect(colour = "black", fill=NA, size=0.2))
 ggsave(paste0(prefix,'_divergent_transcription.pdf'),width=4,height = 3)
-# tss_min <- c(-305,-495)
-# tss_max <- c(120,330)
-# # ctcf_max <-c(130,330,520,700,880,-130,-330,-520,-700,-880)
-# # ctcf_min <- c(-230,-420,-610,-790,230,420,610,790)
-# #plot(1:400,dox_mean[1,],type='l')
-# #---quantify
-# max_local <- function(data,loc){
-#   data <- data %>%
-#     filter(coor>(loc-50) & coor <(loc+50))
-#   return(max(data$value))
-# }
-# min_local <- function(data,loc){
-#   data <- data %>%
-#     filter(coor>(loc-50) & coor <(loc+50))
-#   return(min(data$value))
-# }
-# find_real_coor <- function(data,coor_df,type){
-#   if (type=='max'){
-#     for (i in 1:nrow(coor_df)){
-#       va <- max_local(data,coor_df$cand[i])
-#       coor_df$value[i]<- va
-#       loc<-which(data$value==va)
-#       coor_df$real_coor[i]<-data$coor[loc]
-#     }
-#   }else if (type=='min'){
-#     for (i in 1:nrow(coor_df)){
-#       va <- min_local(data,coor_df$cand[i])
-#       coor_df$value[i]<- va
-#       loc<-which(data$value==va)
-#       coor_df$real_coor[i]<-data$coor[loc]
-#     }
-#   }
-#   return(coor_df)
-# }
-# find_value <- function(data,coor){
-#   data2 <- data.frame('coor'=coor,
-#                       'value'=0)
-#   for (i in 1:nrow(data2)){
-#     loc <- which(data$coor==data2$coor[i])
-#     data2$value[i] <- data$value[loc]
-#   }
-#   data2<-data2%>%
-#     arrange(data2$coor)
-#   return(data2)
-# }
-# #----find coor to quantify
-# all <- dox_mean
-# all$value <- dox_mean$value+h05_mean$value+h1_mean$value
-# cand_max <- data.frame('cand'=c(120,330),
-#                        'value'=0,'real_coor'=0)
-# real_max <- find_real_coor(dox_mean,cand_max,'max')
-# 
-# cand_min <- data.frame('cand'=c(-305,-495),
-#                        'value'=0,'real_coor'=0)
-# real_min <- find_real_coor(dox_mean,cand_min,'min')
-# 
-# dox_max <- find_value(dox_mean,real_max$real_coor)
-# dox_min <- find_value(dox_mean,real_min$real_coor)
-# h05_max <- find_value(h05_mean,real_max$real_coor)
-# h05_min <- find_value(h05_mean,real_min$real_coor)
-# h1_max <- find_value(h1_mean,real_max$real_coor)
-# h1_min <- find_value(h1_mean,real_min$real_coor)
-# find_change <- function(dox_max,h05_max,h1_max){
-#   names(dox_max) <- c('coor','dox')
-#   names(h05_max) <- c('coor','h05')
-#   names(h1_max) <- c('coor','h1')
-#   merge <- merge(dox_max,h05_max)
-#   merge_max <- merge(merge,h1_max)
-#   merge_max$dox_05 <- merge_max$h05/merge_max$dox
-#   merge_max$dox_1 <- merge_max$h1/merge_max$dox
-#   merge_max$h05_1 <- merge_max$h1/merge_max$h05
-#   return(merge_max)
-# }
-# 
-# merge_max <- find_change(dox_max,h05_max,h1_max)
-# merge_min <- find_change(dox_min,h05_min,h1_min)
-# fwrite(merge_max,'tss_unbinding_max.csv')
-# fwrite(merge_min,'tss_unbinding_min.csv')
-# 
